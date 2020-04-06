fa75bc2d5a1d38885d2539a45595da02abbad20e
brave-ping-4201 <h_yazie@yahoo.com>
2020-04-06 07:13:18 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,3 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
-end
-
-def toadstool
-  puts status
+  status = 'Thank you Mario! But Our Princess Is In Another Castle!'
 end
