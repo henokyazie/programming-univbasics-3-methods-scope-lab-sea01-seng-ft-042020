22d87d5bdc50244daaf0af7e5646cc699122c5f7
brave-ping-4201 <h_yazie@yahoo.com>
2020-04-06 07:52:53 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -14,9 +14,3 @@ end
 def link 
   puts "It's Dangerous To Go Alone! Take This."
 end
-
-def all_phrases
-  expect{all_phrases}.to output(/It's-a me, Mario!\n/).to_stdout
-    expect{all_phrases}.to output(/Thank You Mario! But Our Princess Is In Another Castle!\n/).to_stdout
-    expect{all_phrases}.to output(/It's Dangerous To Go Alone! Take This.\n/).to_stdout
-  end 
