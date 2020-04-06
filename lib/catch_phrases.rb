7ea4eaa35b92156f72c097589d10b4ca45e223a1
brave-ping-4201 <h_yazie@yahoo.com>
2020-04-06 08:00:20 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -11,8 +11,8 @@ def toadstool
   puts status
 end
 
-describe "link" do
-  it "puts out 'It's Dangerous To Go Alone! Take This.'" do
+def link 
+   puts "It's Dangerous To Go Alone! Take This."
     expect{link}.to output("It's Dangerous To Go Alone! Take This.\n").to_stdout
   end
 end
