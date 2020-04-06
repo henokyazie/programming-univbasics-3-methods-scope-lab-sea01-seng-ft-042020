d031662b6d06916f126a0102ee122ff326d039fa
brave-ping-4201 <h_yazie@yahoo.com>
2020-04-06 07:31:42 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,10 @@
 def mario
-  phrase = 'It's-a me, Mario!'
+  describe "mario" do
+  it "puts out 'It's-a me, Mario!'" do
+    phrase = "It's-a me, Mario!"
+    expect{mario}.to output("It's-a me, Mario!\n").to_stdout
+  end
+end
   status = 'Thank you Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
